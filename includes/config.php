7321b7fb3cde753fe93fa95f2659fd0afaf7ca4f
ianrohandavis <43388802+ianrohandavis@users.noreply.github.com>
2018-11-19 19:51:06 -0500

    Add files via upload

--- includes/config.php
+++ includes/config.php
@@ -4,11 +4,11 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	// $con = mysqli_connect("localhost", "root", "", "smooth");
+	$con = mysqli_connect("localhost", "root", "", "smooth");
 	// $dbconn = pg_connect("dbname=cavjuiceclients");
-	$con = pg_connect("host=
-ec2-184-72-234-230.compute-1.amazonaws.com port=
-5432 dbname=mary user=utqwckgarmqowa password=d8c00bc1ace96155f0d2bab63931ac0c6961ebcf3ac5e21aa7230bc42feee993");
+// 	$con = pg_connect("host=
+// ec2-184-72-234-230.compute-1.amazonaws.com port=
+// 5432 dbname=mary user=utqwckgarmqowa password=d8c00bc1ace96155f0d2bab63931ac0c6961ebcf3ac5e21aa7230bc42feee993");
 
 	// if(mysqli_connect_errno()) {
 	// 	echo "Failed to connect: " . mysqli_connect_errno();
